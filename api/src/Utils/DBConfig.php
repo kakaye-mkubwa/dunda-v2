23a887d36c6df7331acf184998bf3257c7196a24
kakaye-mkubwa <44920699+kakaye-mkubwa@users.noreply.github.com>
2021-03-26 05:12:58 -0400

    Removed config details

--- api/src/Utils/DBConfig.php
+++ api/src/Utils/DBConfig.php
@@ -6,10 +6,4 @@ class DBConfig{
     const DB_USER="root";
     const DB_PASS="123456";
     const DB_NAME="dunda";
-//    N@T6guq-2*3r
-
-//    const DB_HOST ="";
-//    const DB_USER="dundafoo_admin";
-//    const DB_PASS="N@T6guq-2*3r";
-//    const DB_NAME="dundafoo_dunda";
 }
